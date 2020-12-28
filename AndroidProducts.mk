f4335e7e2589bae6cfb0461f3bfee19a30562a82
jrchintu <harrypetersun@outlook.com>
2020-12-28 11:47:15 +0000

    change du->corvus in bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/du_mido.mk
+    $(LOCAL_DIR)/corvus_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    du_mido-user \
-    du_mido-userdebug \
-    du_mido-eng
+    corvus_mido-user \
+    corvus_mido-userdebug \
+    corvus_mido-eng
