71232f1edf320db4028bb255ba84a26a86746007
DOGE <officialpkkmkb@gmail.com>
2021-03-06 23:40:54 +0530

    It has been depreciated

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,10 +6,6 @@ git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996
 git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_display -b eleven hardware/qcom-caf/msm8996/display
 git clone --depth=1 https://github.com/LineageOS/android_device_qcom_sepolicy -b lineage-18.1-legacy-um device/qcom/sepolicy-legacy-um
 
-#add build choices
-add_lunch_combo aosip_mido-userdebug
-add_lunch_combo aosip_mido-user
-
 #auto clone kernel tree & vendor tree
 git clone --depth=1 https://github.com/Erickeagle3/android_kernel_xiaomi_mido -b lineage-18.1 kernel/xiaomi/mido
 git clone --depth=1 https://github.com/shashank1436/vendor_xiaomi-mido -b r11 vendor/xiaomi
