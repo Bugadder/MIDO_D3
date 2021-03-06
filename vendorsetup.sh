3432d7b646c4a9cba933b6f250cb8b9412f897d4
ramdibaaz <officialpkkmkb@gmail.com>
2021-03-06 19:41:43 +0530

    Add links for auto cloning to vendorsetpu

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+#auto clone hals
+rm -rf device/qcom/sepolicy-legacy-um hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/display
+git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_audio    hardware/qcom-caf/msm8996/audio
+git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_media    hardware/qcom-caf/msm8996/media
+git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_display  hardware/qcom-caf/msm8996/display
+git clone --depth=1 https://github.com/LineageOS/android_device_qcom_sepolicy -b lineage-18.1-legacy-um device/qcom/sepolicy-legacy-um
 
 #add build choices
 add_lunch_combo aosip_mido-userdebug
 add_lunch_combo aosip_mido-user
+
+#auto clone kernel tree & vendor tree & device tree
+git clone --depth=1 https://github.com/Erickeagle3/android_kernel_xiaomi_mido -b lineage-18.1 kernel/xiaomi/mido
+git clone --depth=1 https://github.com/shashank1436/vendor_xiaomi-mido -b r11 vendor/xiaomi
+git clone --depth=1 https://github.com/ramdibaaz/MIDO_D3 -b r11 device/xiaomi/mido
