81847157802dacf9a8db95776afaa8c347405ab4
ramdibaaz <officialpkkmkb@gmail.com>
2021-03-06 19:50:41 +0530

    edit branch names in hals for vendorsetup
    Signed-off-by: ramdibaaz <officialpkkmkb@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #auto clone hals
 rm -rf device/qcom/sepolicy-legacy-um hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/display
-git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_audio    hardware/qcom-caf/msm8996/audio
-git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_media    hardware/qcom-caf/msm8996/media
-git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_display  hardware/qcom-caf/msm8996/display
+git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_audio -b eleven hardware/qcom-caf/msm8996/audio
+git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_media -b eleven hardware/qcom-caf/msm8996/media
+git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_display -b eleven hardware/qcom-caf/msm8996/display
 git clone --depth=1 https://github.com/LineageOS/android_device_qcom_sepolicy -b lineage-18.1-legacy-um device/qcom/sepolicy-legacy-um
 
 #add build choices
