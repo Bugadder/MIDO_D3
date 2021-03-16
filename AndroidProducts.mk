f87cbf5c8d3818d0a36a133e37c26b50656dd6aa
DOGE <officialpkkmkb@gmail.com>
2021-03-17 02:21:31 +0530

    Bringup for potato os (2)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosip_mido.mk
+    $(LOCAL_DIR)/potato_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosip_mido-user \
-    aosip_mido-userdebug \
-    aosip_mido-eng
+    potato_mido-user \
+    potato_mido-userdebug \
+    potato_mido-eng
